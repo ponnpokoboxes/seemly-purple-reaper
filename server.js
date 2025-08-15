47aeeb8a46576bd2a2e747df6aabdb6577cdc7d0
ponnpokoboxes <ponnpokoboxes@gmail.com>
2025-08-15 16:25:31 +0900

    Update server.js
    a little amendment

--- server.js
+++ server.js
@@ -160,18 +160,18 @@ http
 
 let readyIs = "NA";
 
+client.once(Events.ClientReady, (c) => {
+  console.log("Bot準備完了～");
+  readyIs = "Discord Bot is active now\n";
+  client.user.setPresence({ activities: [{ name: "ゲーム補佐" }] });
+});
+
 client.on("debug", (c) => {
   console.log("▼▼debugによる情報▼▼\n" + c);
 });
 
 client.on("warn", (c) => {
   console.log("▼▼warnによる情報▼▼\n" + c);
-});
-
-client.once(Events.ClientReady, (c) => {
-  console.log("Bot準備完了～");
-  readyIs = "Discord Bot is active now\n";
-  client.user.setPresence({ activities: [{ name: "ゲーム補佐" }] });
 });
 
 if (process.env.OPERAS == undefined) {
